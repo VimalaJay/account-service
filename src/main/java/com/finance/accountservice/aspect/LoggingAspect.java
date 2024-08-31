c6108a337c8ef14b10753c48c3357194d151dc74
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-08-31 15:11:58 +0200

    Update and rename LoggingAspect to LoggingAspect.java

---
+++ src/main/java/com/finance/accountservice/aspect/LoggingAspect.java
@@ -0,0 +1,32 @@
+package com.finance.accountservice.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+	
+	@Around("execution(* com.finance.accountservice..*.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		log.info("Entering Method: [" + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName() + "]");
+        Object result;
+        try {
+            result = joinPoint.proceed();
+    		log.info("Exiting Method: [" + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName() + "]");
+        } catch (Throwable error) {
+    		log.info("Exception in Method: [" + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName() + "]");
+            throw error;
+		}
+        return result;
+    }
+	
+}
