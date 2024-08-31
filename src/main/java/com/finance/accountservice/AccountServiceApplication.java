34ffa3bd47a91d41f79534cee47def6275182ba2
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-08-31 15:08:50 +0200

    Update AccountServiceApplication.java

--- src/main/java/com/finance/accountservice/AccountServiceApplication.java
+++ src/main/java/com/finance/accountservice/AccountServiceApplication.java
@@ -2,10 +2,10 @@ package com.finance.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableAspectJAutoProxy
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
