365c0bd97082fbdbd6d812ba83716d914b467e13
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-09-02 16:29:06 +0200

    Update FundTransactionDTO.java

--- src/main/java/com/finance/accountservice/dto/FundTransactionDTO.java
+++ src/main/java/com/finance/accountservice/dto/FundTransactionDTO.java
@@ -2,9 +2,14 @@ package com.finance.accountservice.dto;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class FundTransactionDTO {
 	
 	private Long transactionId;
@@ -12,7 +17,7 @@ public class FundTransactionDTO {
 	private String transactionType;
 	private String accountId;
 	private Double amount;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT")
 	private Instant createdDate;
-	private Instant updatedDate;
 
 }
