25c6d2b4256a0a1e347697d8cc2d02d6a29e4571
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-09-02 16:20:56 +0200

    Update Response.java

--- src/main/java/com/finance/accountservice/util/Response.java
+++ src/main/java/com/finance/accountservice/util/Response.java
@@ -1,6 +1,7 @@
 package com.finance.accountservice.util;
 
-import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,9 +10,11 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class Response {
 	
-	private String message;
-    private HttpStatus httpStatus;
+    private String message;
+    private int statusCode;
+    private Object data;
 
 }
