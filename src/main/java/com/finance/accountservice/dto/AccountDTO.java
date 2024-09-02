8ee803ddeb6d5f6983ea71610d72a24f703dba03
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-09-02 16:28:28 +0200

    Update AccountDTO.java

--- src/main/java/com/finance/accountservice/dto/AccountDTO.java
+++ src/main/java/com/finance/accountservice/dto/AccountDTO.java
@@ -1,13 +1,19 @@
 package com.finance.accountservice.dto;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class AccountDTO implements Serializable {
-	
+
 	/**
 	 * 
 	 */
@@ -16,6 +22,8 @@ public class AccountDTO implements Serializable {
 	private String firstName;
 	private String surName;
 	private Double balance;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT")
+	private Instant createdDate;
 	List<FundTransactionDTO> transactions;
 
 }
