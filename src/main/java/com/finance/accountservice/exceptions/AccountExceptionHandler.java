9a9f91f0e7861be4580fd22604aead0e283510ff
VimalaJay <6406772+VimalaJay@users.noreply.github.com>
2024-09-02 16:26:43 +0200

    Update AccountExceptionHandler.java

--- src/main/java/com/finance/accountservice/exceptions/AccountExceptionHandler.java
+++ src/main/java/com/finance/accountservice/exceptions/AccountExceptionHandler.java
@@ -9,20 +9,24 @@ import com.finance.accountservice.util.Response;
 
 @RestControllerAdvice
 public class AccountExceptionHandler {
-	
+
 	@ExceptionHandler(value = ServiceException.class)
-    public ResponseEntity<Response> customerHandler(
-			ServiceException ex) {
-		Response errorResponse = Response.builder().message(ex.getMessage()).httpStatus(ex.getErrorCode())
+	public ResponseEntity<Response> customerHandler(ServiceException ex) {
+		Response errorResponse = Response.builder().message(ex.getMessage()).statusCode(ex.getErrorCode()).build();
+		return ResponseEntity.ok(errorResponse);
+	}
+
+	@ExceptionHandler(value = AccountAlreadyExistsException.class)
+	public ResponseEntity<Response> accountExistsErrorhandler(Exception ex) {
+		Response errorResponse = Response.builder().message(ex.getMessage()).statusCode(HttpStatus.CONFLICT.value())
 				.build();
 		return ResponseEntity.ok(errorResponse);
 	}
-	
+
 	@ExceptionHandler(value = Exception.class)
-    public ResponseEntity<Response> handler(
-			Exception ex) {
-		Response errorResponse = Response.builder().message(ex.getMessage()).httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-				.build();
+	public ResponseEntity<Response> handler(Exception ex) {
+		Response errorResponse = Response.builder().message(ex.getMessage())
+				.statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
 		return ResponseEntity.ok(errorResponse);
 	}
 
